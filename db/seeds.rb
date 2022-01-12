e2b513520bfb8165744370b8d900476f8d4d35bc
Anthony <tonymich2012@gmail.com>
2022-01-12 09:52:32 -0800

    adds default products to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,9 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+#   Character.create(name: "Luke", movie: movies.
+Product.create(name: "Old Man's War", price: 20, image_url: "", description: "The first installment of the Old Man's War series. Written by John Scalzi.")
+Product.create(name: "Twilight", price: 25, image_url: "", description: "The first installment in a teen book series. Written by Stephanie Meyer.")
+Product.create(name: "Bookmarks", price: 0.75, image_url: "", description: "A placeholder so you'll never y=lose your place in a book.")
+Product.create(name: "reading glasses", price: 7.50, image_url: "", description: "Glasses to help you read! For when your eyes just aren't good enough.")
+Product.create(name: "Comics", price: 8, image_url: "", description: "The best stories of ade=ventures for your growing child!")
