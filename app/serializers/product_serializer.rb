9db594dc0a67e7c785fc0c05d3ef0c8d3664eaca
Anthony <tonymich2012@gmail.com>
2022-01-27 11:38:18 -0800

    adds order, product, and supplier serializers.

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -2,4 +2,5 @@ class ProductSerializer < ActiveModel::Serializer
   attributes :id, :name, :price, :image_url, :description, :tax, :total, :is_discounted?
 
   belongs_to :supplier
+  has_many :images
 end
